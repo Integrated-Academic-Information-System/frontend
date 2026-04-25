19b0052ecf8075830490d91513805fc46077390e
DCDS Karunanayaka <kdchamiladesilva@gmail.com>
2026-04-25 16:21:58 +0530

    refactor: clean up Stack component usage in RootLayout for better readability
    Disabled the default header provided by Expo Router by setting
    `headerShown: false` in the root Stack layout.
    
    This removes the unwanted "index" title and top white header bar,
    ensuring only the custom mobile UI is displayed across all screens.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,16 @@
 import './global.css';
-import {Stack} from "expo-router";
-import {SafeAreaProvider} from "react-native-safe-area-context";
+import { Stack } from "expo-router";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function RootLayout() {
     return (
         <SafeAreaProvider>
-            <Stack>
-                <Stack.Screen name="(tabs)"
-                              options={{
-                                  headerShown: false,
-                              }}/>
+            <Stack
+                screenOptions={{
+                    headerShown: false, // 🔥 disable for ALL screens
+                }}
+            >
+                <Stack.Screen name="(tabs)" />
             </Stack>
         </SafeAreaProvider>
     );
